e7ec5956ed02703dc7be65bf35d8260f4ce7bccc
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-10 16:47:02 -0700

    remove const root

--- src/index.js
+++ src/index.js
@@ -4,5 +4,5 @@ import App from './App';
 import './index.css';
 
 
-const root = ReactDOM.render(<App/>, document.querySelector("#root"));
+ReactDOM.render(<App/>, document.querySelector("#root"));
 
