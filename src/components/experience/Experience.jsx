60dfa8d6f2eb54ed0a28dc33f4791d68ba4a56a8
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-11 14:15:36 -0700

    Add section ids

--- src/components/experience/Experience.jsx
+++ src/components/experience/Experience.jsx
@@ -4,7 +4,7 @@ import './experience.css';
 const Experience = () => {
   return (
     <section id='experience'>Experience</section>
-  )
+  );
 }
 
-export default Experience
+export default Experience;
