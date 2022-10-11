ac70392b35645eb2e80c97e3294f842b01b4106d
Liam Cammpbell <liam22campbell@gmail.com>
2022-10-11 15:08:31 -0700

    create about section materials.

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
 import './about.css';
+import ME from '../../assets/IMG-7258.jpg';
+import {FaAward} from 'react-icons/fa';
+// import {FiUsers} from 'react-icons/fi';
+import {VscFolderLibrary} from 'react-icons/vsc';
 
 const About = () => {
   return (
-    <section id='about'>About</section>
+    <section id='about'>
+      <h5>Get To Know</h5>
+      <h2>About Me</h2>
+      <div className="container about__container">
+        <div className='about__me'>
+          <div className='about__me-image'>
+            <img src={ME} alt='Me in a green shirt smiling infront of some planters on a light green background.'/>
+          </div>
+        </div>
+        <div className='about__content'>
+          <div className='about__cards'>
+            <article className='about__card'>
+              <FaAward className='about__icon'/>
+              <h5>Experience</h5>
+              <small>Working experience</small>
+            </article>
+            {/* <article className='about__card'>
+              <FiUsers className='about__icon'/>
+              <h5>Coworkers</h5>
+              <small>Working experience</small>
+            </article> */}
+            <article className='about__card'>
+              <VscFolderLibrary className='about__icon'/>
+              <h5>Projects</h5>
+              <small>50+ Completed Projects</small>
+            </article>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Earum nostrum perspiciatis veniam libero magnam illo architecto ipsam ratione repellendus neque natus molestias animi sapiente magni, ipsa iste. Saepe, tenetur. Dolore!</p>
+          <a href='#contact' className='btn btn-primary'>Let's Talk</a>
+        </div>
+      </div>
+    </section>
   )
 }
 
