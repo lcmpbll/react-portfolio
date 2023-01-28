5d9cb72a431eafbafaec412aa81b4998432c0b41
Liam Cammpbell <liam22campbell@gmail.com>
2023-01-28 14:53:29 -0800

    Remove unused links

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -13,14 +13,14 @@ const Footer = () => {
         <li><a href='#about'>About</a></li>
         <li><a href='#experience'>Experience</a></li>
         <li><a href='#portfolio'>Portfolio</a></li>
-        <li><a href='#testimonials'>Testimonials</a></li>
+        {/* <li><a href='#testimonials'>Testimonials</a></li> */}
         <li><a href='#contact'>Contact</a></li>
       </ul>
-      <div className='footer__socials'>
+      {/* <div className='footer__socials'>
         <a href='https://facebook.com'><FaFacebookF/></a>
         <a href='https://instagram.com'><FiInstagram/></a>
         <a href='https://twitter.com'><IoLogoTwitter/></a>
-      </div>
+      </div> */}
       <div className="footer__copyright">
         <small>&copy; Liam Campbell All rights reserved.</small>
       </div>
